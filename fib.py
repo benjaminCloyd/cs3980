274613237ca1963f422ea774148cb31bf8bbb995
benjaminCloyd <benjamin-cloyd@uiowa.edu>
2026-02-03 22:12:51 -0600

    fib

--- fib.py
+++ fib.py
@@ -1,10 +1,29 @@
 # fib.py
+from functools import lru_cache, wraps
+from time import perf_counter
+
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = perf_counter()
+        result = func(*args, **kwargs)
+        end = perf_counter()
+        print(f"Finished in {end - start:.8f}s: f({args[0]}) -> {result}")
+        return result
+
+    return wrapper
 
 
 @lru_cache
 @timer
 def fib(n: int) -> int:
-    pass
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
 
 
 if __name__ == "__main__":
